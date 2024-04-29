c68baf76fb02d842820c5af5bd1796b986d958d1
et120 <e.trotter120@gmail.com>
2024-04-29 11:56:57 -0700

    navbar fix + start profile page

--- src/app/components/NavbarComponent.tsx
+++ src/app/components/NavbarComponent.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "next/image";
 import Logo from '@/app/assets/Logo.png'
 
 const NavbarComponent = () => {
-    let isLoggedIn = false;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userColor, setUserColor] = useState('#57CDFF');
 
   return (
     <nav className='h-24 bg-gradient-to-r from-[#AEE6D9] to-[#3EBE9F] flex justify-between items-center px-5'>
-      <Image src={Logo} alt="logo" height={80} className='pb-2'/>
-      <div className={`bg-[#57CDFF] border border-black rounded-full h-12 w-12 ${isLoggedIn ? 'block' : 'hidden'}`}></div>
+      <Image src={Logo} alt="logo" height={80} className='pb-3'/>
+      <div className={`bg-[${userColor}] border border-black rounded-full h-12 w-12 ${isLoggedIn ? 'block' : 'hidden'}`}></div>
     </nav>
   )
 }
