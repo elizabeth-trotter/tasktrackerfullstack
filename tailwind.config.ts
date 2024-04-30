d273637194c26bce6e89c8665bc4140d5d0dc945
itzteeler <tylernguyen518@gmail.com>
2024-04-30 11:51:43 -0700

    CloseModal/NewTaskComp/TaskCardComp/Modal

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "tailwindcss";
-
+const flowbite = require("flowbite-react/tailwind");
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    flowbite.content()
   ],
   theme: {
     extend: {
